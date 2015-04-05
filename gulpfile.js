3c2a4abf61cd2c8215034494a38c74327017b3ce
wzaghal <wisamzaghal@gmail.com>
2015-04-05 16:56:31 -0400

    setup tests

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('mocha-ci', function (cb) {
+  return gulp.src('lib/**/*.test.js', {
+      read: false
+    })
+    .pipe(mocha({
+      reporter: 'min'
+    }));
+});
+
+gulp.task('mocha-watch', function () {
+  gulp.watch('lib/**/*.js', ['mocha-ci']);
+});
+
+gulp.task('default',['mocha-watch']);
