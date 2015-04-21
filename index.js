a0bccf204aeffc03511bf16218d86e0538e8ced2
wzaghal <wisamzaghal@gmail.com>
2015-04-20 22:35:06 -0400

    determine that results are files

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var argv = require('yargs').argv;
 var packageFinder = require('./lib/package-finder.js');
-packageFinder.exec(argv, './sampleProject/');
+packageFinder.exec(argv, './tests/sampleProject/');
---
+++ tests/sampleProject/app/api-router.js
@@ -0,0 +1,5 @@
+var router = require('express').Router;
+var mock = require('./mock');
+var _ = require('underscore');
+var loggly = require('loggly');
+var inspect = require('util').inspect;
