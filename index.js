63ee4de8fa66081c32feb627794ce3e3fb409a9b
wzaghal <wisamzaghal@gmail.com>
2015-04-28 22:28:17 -0400

    prepare for first release

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var argv = require('yargs').argv;
 var packageFinder = require('./lib/package-finder.js');
-packageFinder.exec(argv, './tests/sampleProject/');
+
+//TODO - use path.resolve/path.join to handle all directories/files
+packageFinder.exec(argv, __dirname + '/tests/sampleProject/');
