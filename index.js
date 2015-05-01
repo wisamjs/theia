0c7e52641b12f44ccf676dbe7e4d91bd17fb3a8c
wzaghal <wisamzaghal@gmail.com>
2015-04-30 23:23:54 -0400

    set packge to cli

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
+
 var argv = require('yargs').argv;
 var packageFinder = require('./lib/package-finder.js');
-var path = require('path');
 
-packageFinder.exec(argv, path.join(__dirname));
+packageFinder.exec(argv, process.cwd());
